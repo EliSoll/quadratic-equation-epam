d76d569a142f81f2dc5fe4b819847a1106b08648
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-07-12 11:29:31 +0300

    test 1

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,9 +11,19 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        double d = (b*b) - (4*a*c);
+        double sqrt = Math.sqrt(d);
+        if (d > 0) {
+            var result1 = (-b - sqrt)/(2*a);
+            var result2 = (-b + sqrt)/(2*a);
+            System.out.println(result1 + " " + result2);
+        }
+        else if(d == 0) {
+            var result1 = -(b/2*a);
+            System.out.println(result1);
+        }
+        else if(d < 0){
+            System.out.println("no roots");
+        }
     }
-
 }
